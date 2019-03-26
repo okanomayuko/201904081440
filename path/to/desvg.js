f5d083dad4e04d6aa05e41b08f9efda86afdd991
okanomayuko <cyclosweet@yahoo.co.jp>
2019-03-26 15:50:43 +0900

    desvgtest

---
+++ path/to/desvg.js
@@ -0,0 +1,57 @@
+(function () {
+    var desvg = function (selector, removeinlinecss) {
+
+        var images = document.querySelectorAll(selector),
+            len = images.length;
+
+        while (len--) {
+            var img = images[len],
+                imgID = img.id,
+                imgClasses = img.getAttribute('class'),
+                imgURL = img.getAttribute('src'),
+                imgParent = img.parentNode,
+                removeinlinecss = removeinlinecss || null,
+                svg,
+                paths,
+                xhr;
+
+            xhr = new XMLHttpRequest();
+
+            xhr.open('GET', imgURL, true);
+
+            xhr.onload = function () {
+                var xml = xhr.responseXML;
+
+                if (!xml) {
+                    return;
+                }
+
+                svg = xml.documentElement;
+
+                paths = svg.querySelectorAll('path');
+
+                if (imgID) {
+                    svg.id = imgID;
+                }
+
+                if (imgClasses) {
+                    svg.setAttribute('class', imgClasses + ' replaced-svg');
+                }
+
+                if (removeinlinecss) {
+                    for (var i = 0; i < paths.length; i++) {
+                        paths[i].removeAttribute('style');
+                    }
+                }
+
+                svg.removeAttribute('xmlns:a');
+
+                imgParent.appendChild(svg);
+                imgParent.removeChild(img);
+            };
+
+            xhr.send();
+        }
+    };
+    window.deSVG = desvg;
+})();
