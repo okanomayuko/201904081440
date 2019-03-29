413584d059a7187bf99cf3e411f85709f1b86e0b
okanomayuko <cyclosweet@yahoo.co.jp>
2019-03-29 21:46:34 +0900

    Update navscroll.js

--- script/navscroll.js
+++ script/navscroll.js
@@ -5,7 +5,7 @@ $(function() {
 
   $win.on('load scroll', function() {
     var value = $(this).scrollTop();
-    if ( value > 500 ) {
+    if ( value > 100 ) {
       $cloneNav.addClass(showClass);
     } else {
       $cloneNav.removeClass(showClass);
